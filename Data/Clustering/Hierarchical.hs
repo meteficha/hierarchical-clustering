29feee77c7e015b1aaa9394ba8e1ae4c4df05cd5
Felipe Lessa <felipe.lessa@gmail.com>
2010-08-02 23:37:23 +0000

    Change 'Clustered' name to 'Dendrogram'.

--- Data/Clustering/Hierarchical.hs
+++ Data/Clustering/Hierarchical.hs
@@ -1,5 +1,5 @@
 module Data.Clustering.Hierarchical
-    (Clustered(..)
+    (Dendrogram(..)
     ) where
 
 import Control.Applicative
@@ -8,21 +8,21 @@ import Data.Monoid
 import Data.Traversable
 
 -- | Data structure for storing hierarchical clusters.
--- Shamelessly inspired by @bio@ package.
-data Clustered score datum =
+data Dendrogram score datum =
     Leaf datum
-  | Branch score (Clustered score datum) (Clustered score datum)
+  | Branch score (Dendrogram score datum) (Dendrogram score datum)
     deriving (Eq, Ord, Show)
 
-instance Functor (Clustered score) where
+-- | Does not recalculate the scores!
+instance Functor (Dendrogram score) where
     fmap f (Leaf d)         = Leaf (f d)
     fmap f (Branch s c1 c2) = Branch s (fmap f c1) (fmap f c2)
 
-instance Foldable (Clustered score) where
+instance Foldable (Dendrogram score) where
     foldMap f (Leaf d)         = f d
-    foldMap f (Branch s c1 c2) = foldMap f c1 `mappend` foldMap f c2
+    foldMap f (Branch _ c1 c2) = foldMap f c1 `mappend` foldMap f c2
 
-instance Traversable (Clustered score) where
+instance Traversable (Dendrogram score) where
     traverse f (Leaf d)         = Leaf <$> f d
     traverse f (Branch s c1 c2) = Branch s <$> traverse f c1 <*> traverse f c2
 
